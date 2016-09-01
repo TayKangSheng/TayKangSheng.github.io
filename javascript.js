772bc7975d2499b0f23c278b050b1b2663736bdd
Tay Kang Sheng <kangsheng@gmail.com>
2016-09-01 15:31:33 +0800

    ..

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
 window.onload = function() {
-	// console.log("onload");
 	var container = document.getElementById("container");
-	// console.log(container);
 	var hash = window.location.hash.substring(1);
 	if (!hash){
 		document.getElementById("container").getElementsByTagName("div")[0].style.display = "inline-block";
@@ -24,5 +22,9 @@ function hashchange(){
 	var divs = document.getElementById("container").getElementsByTagName("div");
 	for (var i=0 ; i<divs.length ; i++){ divs[i].style.display="none"; }
 	var hash = window.location.hash.substring(1);
-	document.getElementById(hash).style.display = "inline-block";
+	if (!hash){
+		document.getElementById("container").getElementsByTagName("div")[0].style.display = "inline-block";
+	}else {
+		document.getElementById(hash).style.display = "inline-block";
+	}
 }
